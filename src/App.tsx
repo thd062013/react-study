a5de7a99b381165ff8693c893a49b45064872a76
Ryuintae <dlsxochlrg@ejbt.co.kr>
2024-08-11 18:39:59 +0900

    git 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface testProps{
+  name : string,
+  age : number
+}
+function App(props : testProps) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reloaaaad.aa
+          {props.name}
         </p>
         <a
           className="App-link"
